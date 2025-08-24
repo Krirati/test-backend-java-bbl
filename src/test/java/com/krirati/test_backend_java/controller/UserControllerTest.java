94cd990ccc51306e4ba40e7e845850fd7db4a69b
krirati <kriratik@odds.team>
2025-08-24 15:13:42 +0700

    init file test controller

---
+++ src/test/java/com/krirati/test_backend_java/controller/UserControllerTest.java
@@ -0,0 +1,18 @@
+package com.krirati.test_backend_java.controller;
+
+import com.krirati.test_backend_java.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+}
